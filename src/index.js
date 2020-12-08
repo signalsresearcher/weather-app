d8742994df99effeb325a0f7caa5b7d42e3c8821
Larissa <signalsresearcher@gmail.com>
2020-12-07 23:17:25 -0500

    removed comments added styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-//## Initialize variables ##
 let celsiusTemperature = null;
 let form = document.querySelector("#citySelect");
 form.addEventListener("submit", submitSearch);
@@ -12,39 +11,6 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 let searchForm = document.querySelector("#citySelect");
 searchForm.addEventListener("submit", submitSearch);
 
-// let dateElement = document.querySelector("#date");
-// let currentTime = new Date();
-// dateElement.innerHTML = formatDate(currentTime);
-
-// function formatDate(timestamp) {
-//   let date = new Date(timestamp);
-//   let days = [
-//     "Sunday",
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday"
-//   ];
-//   var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-//   let day = days[date.getDay()];
-//   return `${day} ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()} ${formatHours(timestamp)}`;
-// }
-
-// function formatHours(timestamp) {
-//   let date = new Date(timestamp);
-//   let hours = date.getHours();
-//   if (hours < 10) {
-//     hours = `0${hours}`;
-//   }
-//   let minutes = date.getMinutes();
-//   if (minutes < 10) {
-//     minutes = `0${minutes}`;
-//   }
-//   return `${hours}:${minutes}`;
-// }
 
 function showTemperature(response) {
   console.log('showTemperature()');
@@ -53,15 +19,20 @@ function showTemperature(response) {
   let iconElement = document.querySelector("#starry");
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   document.querySelector("#place").innerHTML = response.data.name;
+   let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `Wind: ${response.data.wind.speed}m/sec`;
+  let humidElement = document.querySelector("#humidity");
+  humidElement.innerHTML = `Humidity: ${response.data.main.humidity} %`;
   document.querySelector("#tempo").innerHTML = Math.round(
     response.data.main.temp
   );
 }
 
+
+
 function searchCity(city) {
   console.log("searchCity():", city);
 
-  //## Check if city is blank or undefined and show error message ##
   if (city == "" || typeof city == "undefined") {
     Swal.fire({
       title: '<strong>Error</strong>',
@@ -75,12 +46,10 @@ function searchCity(city) {
     let units = "metric";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-    // axios.get(apiUrl).then(showTemperature);
     axios.get(apiUrl).then(function (response) {
       console.log(response);
       showTemperature(response);
 
-      // apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
       apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${response.data.coord.lat}&lon=${response.data.coord.lon}&excluse={minutely,alerts}&units=${units}&appid=${apiKey}`;
 
       axios.get(apiUrl).then(function(response) {
@@ -98,7 +67,6 @@ function searchCity(city) {
     }).catch(function (error) {
       let msg = JSON.parse(error.request.response);
 
-      //## Added error check is city is not found ##
       Swal.fire({
         title: '<strong>Error</strong>',
         icon: 'error',
@@ -110,9 +78,8 @@ function searchCity(city) {
 
   }
 }
-//## Call searchCity() on load to get values ##
-searchCity('Quito, Equador');
 
+searchCity('Quito, Ecuador');
 
 
 function submitSearch(event) {
@@ -131,13 +98,11 @@ function displayFahrenheitTemperature(event) {
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
 
-  //let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   let fahrenheitTemperature = degreesCelsiusToFahrenheit(celsiusTemperature);
   let temperatureElement = document.querySelector("#tempo");
 
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 
-  //## Get all i temp elem and display necessary ones ##
   hideTempI();
   var tempFahArr = document.querySelectorAll('.card-body .fah');
   for (let i = 0; i < tempFahArr.length; i++) {
@@ -146,7 +111,7 @@ function displayFahrenheitTemperature(event) {
 
 }
 
-// ## Added degrees celsius to fahrenheit reusable function ##
+
 function degreesCelsiusToFahrenheit(val) {
   return Math.round((val * 9) / 5 + 32);
 }
@@ -165,7 +130,7 @@ function displayCelsiusTemperature(event) {
   let temperatureElement = document.querySelector("#tempo");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 
-  //## Get all i temp elem and display necessary ones ##
+  
   hideTempI();
   var tempCelArr = document.querySelectorAll('.card-body .cel');
   for (let i = 0; i < tempCelArr.length; i++) {
@@ -187,44 +152,17 @@ function displayForecast(response) {
   console.log('displayForecast()');
   console.log(response);
 
-  // ## Moved forecastElement inside the loop ##
-  //let forecastElement = document.querySelector(".card.text-center");
-  // forecastElement.innerHTML = null;
+
   let forecast = null;
 
-  // ## Adjusted the for loop, see below ##
-  /*   
-  for (let index = 0; index < 5; index++) {
-    forecast = response.data.list[index];
-    forecastElement.innerHTML += `
-    <div class="card-header">
-      <h5>
-        ${formatHours(forecast.dt * 1000)}
-      </h5>
-      <img
-        src="http://openweathermap.org/img/wn/${
-          forecast.weather[0].icon
-        }@2x.png"
-      />
-      <div class="p.card-text">
-        <strong>
-          ${Math.round(forecast.main.temp_max)}°
-        </strong>
-        ${Math.round(forecast.main.temp_min)}°
-      </div>
-    </div>
-  `;
-  }
-  */
 
   for (let index = 0; index < 5; index++) {
     forecast = response.data.daily[index];
     console.log('forecast:', forecast);
 
-    //Get card element based on loop index + 1 (e.g. n1, n2, n3..)
     let forecastElement = document.querySelector(".card.n" + (index + 1));
     var date = getEditedTime(forecast.dt, response.data.timezone, false);
-    // console.log(date);
+  
 
     forecastElement.innerHTML = `
       <h5 class="card-header">
@@ -249,10 +187,7 @@ function displayForecast(response) {
   `;
   }
 
-  /* 
-  Notes:
-  Don't use ° for degree symbol, use &deg; html symbol instead. This will ensure that the symbol will display correctly on any device.
-  */
+
 
   document.querySelector('#dateBox').style.display = "block";
   document.querySelector('#preloadBox').style.display = "none";
